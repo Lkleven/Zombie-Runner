9d1baae057afa5f0c069b8fea825371884e76394
Lars Ivar Kleven <likleven@gmail.com>
2017-02-17 21:09:47 +0100

    Player able to respawn

---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public bool respawn = false;
+	public Transform playerSpawnPoints; // The parent of the spawn points
+
+	private Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+
+	}
+	
+	// Update is called once per frames
+	void Update (){
+		if (respawn) {
+			respawn = false;
+			Respawn ();			
+		}
+	
+	}
+
+	private void Respawn(){
+		int randomSpawnNumber = Random.Range (1, spawnPoints.Length);
+		transform.position = spawnPoints [randomSpawnNumber].transform.position;
+	}
+
+}
